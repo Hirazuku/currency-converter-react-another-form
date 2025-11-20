1f61d86cfd7ed917db739d6168a0aac59750c774
Michał Kowalczyk <michalkowal3k@gmail.com>
2025-11-20 17:31:03 +0100

    styled for App.js media width added

--- src/App/styled.js
+++ src/App/styled.js
@@ -4,5 +4,9 @@ import {theme} from "../theme";
 export const StyledApp = styled.div`
     background-color: ${({ theme }) => theme.colors.white};
     border-color: ${({ theme }) => theme.colors.black};
-    border-radius: 5px
+    border-radius: 5px;
+
+    @media(max-width: ${({ theme }) => theme.breakpoints.mobile}px) {
+    width: 100%;
+    margin: 5px;}
 `
