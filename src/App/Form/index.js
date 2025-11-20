601f9ad3b92e5e74deeced8ccb92191a61014bfb
Michał Kowalczyk <michalkowal3k@gmail.com>
2025-11-20 10:32:52 +0100

    Loading and Failure applied

--- src/App/Form/index.js
+++ src/App/Form/index.js
@@ -1,27 +1,24 @@
 import { useState } from 'react';
-import { currencies } from "../currencies";
+import { currencies, useRatesData } from "../currencies";
 import { Result } from "./Result";
-import { FormInclusion, FormTitle, FormField, FormButton } from "./styled";
-
-
+import { FormInclusion, FormTitle, FormField, FormButton, Loading, Failure } from "./styled";
 
 export const Form = () => {
 
     const [amount, setAmount] = useState("");
     const [currency, setCurrency] = useState(currencies[0].short);
     const [result, setResult] = useState();
+    const ratesData = useRatesData();
 
     const calculateResult = (currency, amount) => {
-        const rate = currencies
-            .find(({ short }) => short === currency)
-            .rate;
+        const rate = ratesData.rates[currency];
 
         setResult({
             sourceAmount: +amount,
-            targetAmount: amount / rate,
+            targetAmount: amount * rate,
             currency,
         });
-    } 
+    }
 
     const onFormSubmit = (event) => {
         event.preventDefault();
@@ -32,41 +29,58 @@ export const Form = () => {
             <FormInclusion>
                 Policz wartość:
             </FormInclusion>
+            {ratesData.state === "loading"
+                ? (
+                    <Loading>
+                        Proszę czekać, <b>Pobieram aktualne kursy z Europejskiego Banku Centralnego</b>
+                    </Loading>
+                )
+                : (
+                    ratesData.state === "error"
+                        ? (
+                            <Failure>
+                                Ups, coś poczło nie tak. Sprawdź czy sąsiad nie urwał światłowodu
+                            </Failure>
+                        ) :
+                        (<>
+                            <FormTitle>Podaj kwotę do przeliczenia w PLN</FormTitle>
 
-            <FormTitle>Podaj kwotę do przeliczenia w PLN</FormTitle>
+                            <form onSubmit={onFormSubmit}>
+                                <FormField
+                                    value={amount}
+                                    type="number"
+                                    onChange={({ target }) => setAmount(target.value)}
+                                    placeholder="Wpisz kwotę: "
+                                    required
+                                    step="0.01"
+                                />
 
-            <form onSubmit={onFormSubmit}>
-                <FormField
-                    value={amount}
-                    type="number"
-                    onChange={({ target }) => setAmount(target.value)}
-                    placeholder="Wpisz kwotę: "
-                    required
-                    step="0.01"
-                />
+                                <FormField
+                                    as="select"
+                                    value={currency}
+                                    onChange={({ target }) => setCurrency(target.value)}
+                                >
+                                    {currencies.map((currency => (
+                                        <option
+                                            key={currency}
+                                            value={currency}>
+                                            {currency}
+                                        </option>
+                                    )))}
+                                </FormField>
+                                <FormButton>Przelicz</FormButton>
 
-                <FormField
-                    as="select"
-                    value={currency}
-                    onChange={({ target }) => setCurrency(target.value)}
-                >
-                    {currencies.map((currency => (
-                        <option
-                            key={currency.short}
-                            value={currency.short}>
-                            {currency.name}
-                        </option>
-                    )))}
-                </FormField>
-                <FormButton>Przelicz</FormButton>
-
-                <FormInclusion>
-                    <div>
-                        Kwota wynosi:
-                    </div>
-                </FormInclusion>
-                <Result result={result} />
-            </form>
+                                <FormInclusion>
+                                    <div>
+                                        Kwota wynosi:
+                                    </div>
+                                </FormInclusion>
+                                <Result result={result} />
+                            </form>
+                        </>
+                        )
+                )
+            }
         </>
     )
 };
--- src/App/currencies.js
+++ src/App/currencies.js
@@ -51,7 +51,6 @@ export const useRatesData = () => {
                     rates,
                     date,
                 });
-
             }
             catch {
                 setRatesData({
